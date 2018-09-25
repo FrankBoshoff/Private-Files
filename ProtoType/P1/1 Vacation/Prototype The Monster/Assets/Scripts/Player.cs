2281655158f42ecfc7a2e38432efb23ad4f6cd77
Frank <Frank_Boshoff@live.nl>
2018-09-26 00:02:36 +0200

    derp
    hoi

---
+++ ProtoType/P1/1 Vacation/Prototype The Monster/Assets/Scripts/Player.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public Vector3 moveV;
+    public int speed;
+    public int kill;
+    public int mana;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Move();
+        Shoot();
+	}
+
+    void Move()
+    {
+        moveV.x = Input.GetAxis("Horizontal");
+        moveV.y = Input.GetAxis("Vertical");
+        transform.Translate(moveV * speed * Time.deltaTime);
+    }
+
+    void Shoot()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            print("Smash");
+        }
+        else
+        {
+            print("Not Enough mana");
+        }
+    }
+}
