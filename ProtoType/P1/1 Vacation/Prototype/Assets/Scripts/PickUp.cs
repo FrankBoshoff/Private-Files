d141e171b99994843f74c74da85d247d0c14af63
Frank <Frank_Boshoff@live.nl>
2018-09-26 00:02:30 +0200

    Yes
    no

---
+++ ProtoType/P1/1 Vacation/Prototype/Assets/Scripts/PickUp.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour {
+
+    public GameObject manager;
+    public GameObject other;
+
+	void OnTriggerEnter (Collider ok)
+    {
+        print("OK");
+        if (ok.gameObject.tag == "Player")
+        {
+            print("hii");
+            Destroy(other);
+            Destroy(gameObject);
+        }
+	}
+	
+}
---
+++ ProtoType/P1/1 Vacation/Prototype/Assets/Scripts/Player.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public Vector3 movePos;
+    public float walkSpeed;
+    private float movPlyrX;
+    private float movPlyrY;
+
+    private CharacterController controller;
+
+    private float verticalVelocity;
+    private float gravity = 14.0f;
+    private float jumpForce = 10.0f;
+
+
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+	void FixedUpdate ()
+    {
+        Movement(movePos, walkSpeed);
+        Jump();
+	}
+
+    public void Movement(Vector3 mover, float speed)
+    {
+        mover.x = Input.GetAxis("Horizontal");
+        mover.z = Input.GetAxis("Vertical");
+        transform.Translate(mover * speed * Time.deltaTime);
+
+    }
+
+
+
+    private void Jump()
+    {
+        if (controller.isGrounded)
+        {
+            verticalVelocity = -gravity * Time.deltaTime;
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                verticalVelocity = jumpForce;
+            }
+        }
+        else
+        {
+            verticalVelocity -= gravity * Time.deltaTime;
+        }
+        Vector3 moveVector = new Vector3(0, verticalVelocity, 0);
+        controller.Move(moveVector * Time.deltaTime);
+    }
+
+}
