4b4e8aa7ff0adabce71d01354fa75e4edde922e3
Frank <Frank_Boshoff@live.nl>
2018-10-16 12:58:29 +0200

    Roulette
    stuff

---
+++ ProtoType/P1/1 Vacation/Prototype/Assets/Scripts/5e/BallVel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallVel : MonoBehaviour {
+
+    public float speed;
+    Rigidbody rb;
+    public bool restart;
+
+	void Start ()
+    {
+        speed = Random.Range(5f, 14f);
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = new Vector3(0, 0, speed);
+    }
+
+    private void Update()
+    {
+        if(restart == true)
+        {
+            print("destroyed");
+            Destroy(gameObject, 3);
+        }
+    }
+}
---
+++ ProtoType/P1/1 Vacation/Prototype/Assets/Scripts/5e/RoRotator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoRotator : MonoBehaviour {
+
+    public float rotSpeed = 25f;
+    //private float startPos;
+
+	// Use this for initialization
+	/*void Start () {
+        startPos = Random.Range(0f, 360f);
+        transform.Rotate(0,0,startPos);
+	}
+	*/
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(0, 0,-rotSpeed * Time.deltaTime);
+	}
+}
